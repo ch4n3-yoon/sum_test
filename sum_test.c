b10ab81408660f0f66c5971c5cf18099d49d2e2f
chaneyoon <chaneyoon@gmail.com>
2017-07-07 21:58:11 +0900

    fix bug in sum, update .gitignore

--- sum_test.c
+++ sum_test.c
@@ -2,7 +2,11 @@
 
 int main(int argc, char * argv[]) {
 	
-	int i, sum;
+	int i, sum = 0;
+	
+	char track[] = "취약점"; 
+	char name[] = "윤석찬";
+	printf("[bob6][%s]sum_test[%s]\n", track, name);	
 	
 	for(i=0; i<=100; i++) {
 		sum += i;
